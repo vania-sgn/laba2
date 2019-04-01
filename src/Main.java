152481ec61496836a2395f69e483b68ac4cd5f9d
vania-sgn <Skladchikov.vania@gmail.com>
2019-04-01 22:25:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество строк:");
+        int Rows = scanner.nextInt();
+        System.out.println("Введите количество столбиков:");
+        int Сolumns = scanner.nextInt();
+        for(int i =0;i<=Rows;i++){
+            for(int j =0; j<=Сolumns;j++)
+            {
+
+            }
+        }
+
+
+
+    }
+
+}
